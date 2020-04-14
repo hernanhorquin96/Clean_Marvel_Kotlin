10ebbc0e675159e0bf11563f3760b012790f7e24
Hernan Horquín <hernan.horquin@globant.com>
2020-04-14 16:51:45 -0300

    Remove white lines

--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/domain/usecase/StoreCharactersUseCase.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/domain/usecase/StoreCharactersUseCase.kt
@@ -3,7 +3,6 @@ package com.puzzlebench.clean_marvel_kotlin.domain.usecase
 import com.puzzlebench.clean_marvel_kotlin.domain.contracts.CharacterRepository
 import com.puzzlebench.clean_marvel_kotlin.domain.model.Character
 
-
 class StoreCharactersUseCase(private val storeCharactersService: CharacterRepository.Store) {
     operator fun invoke(characters: List<Character>) {
         storeCharactersService.saveCharactersDB(characters)
